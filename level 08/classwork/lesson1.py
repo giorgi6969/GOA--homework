17dba5a969e84c024112d7173cc01bd6ee0fb54f
giorgi6969 <gsulaqvelidz15@gmail.com>
2024-07-04 21:44:36 +0400

    second commit

---
+++ level 08/classwork/lesson1.py
@@ -0,0 +1,24 @@
+name = "alexandre"
+
+print(name)
+
+my_name = "giorgi"
+
+print(my_name)
+
+name = "dato"
+print(name)
+
+first_number = 10
+second_number = 5
+
+Sum = first_number + second_number
+print(sum)
+
+
+
+სახელი = "გიორგი"
+
+გვარი = "სულაქველიძე"
+
+print(სახელი + str(გვარი))
---
+++ level 08/homework/lesson1.py
@@ -0,0 +1,36 @@
+#homework 1
+lemur = 25
+lemur_mango = 10
+
+print("lemur ate", 25-10, "mango")
+
+#homework 2
+print("Hello world!")
+
+
+#homework 3
+x = 5
+y = 16
+print(x + y)
+
+
+#homework 4
+a = 5
+b = 2
+print(a / b)
+
+#homework 5
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#homework 6
+
+age = 25
+print(age + 5)
+
+
+
+
