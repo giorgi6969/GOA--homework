bdb10fdc6b31531bf8536d94773f4a68c3d8094b
giorgi6969 <qweqwe@gmail.com>
2024-08-01 17:39:23 +0400

    first commit

---
+++ level 12/homework/lesson1.py
@@ -0,0 +1,32 @@
+# homework 1
+#string = ტეცსტური მონაცემი
+#integer = მთელი რიცხვი
+#float = ათწილადი
+
+
+#homework 2
+#წაიკითხე შეცდომის შეტყობინება ყურადღებით. ეს ხშირად გაწვდოს კონკრეტულ ინფორმაციას იმის შესახებ, რა იწვევს პრობლემას.
+
+
+#homework 3
+#first_name = "John"
+#last_name = "Doe"
+#full_name = first_name + " " + last_name
+
+
+#str1 = "გამარჯობა"
+#str2 = "მსოფლიო"
+#result = str1 + " " + str2
+#print(result)  # "გამარჯობა მსოფლიო"
+
+
+#variable = "კითხვა"
+#additional_text = "რა არის ეს?"
+#result = "ამ წუთში" + " " + variable + " " + additional_text
+#print(result)  # "ამ წუთში კითხვა რა არის ეს?"
+
+#variable = "რა"
+#additional_text = "გქვია შენ?"
+#result = "ბიჭო" + " " + variable + " " + additional_text
+#print(result)  # "ამ წუთში კითხვა რა არის ეს?"
+
