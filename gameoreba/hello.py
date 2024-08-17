fa9aa6e72cdf97395971f15e70925de2a26c141d
giorgi6969 <qweqwe@gmail.com>
2024-08-17 14:28:41 +0400

    second commit

---
+++ gameoreba/hello.py
@@ -0,0 +1,20 @@
+print("21")
+print("hello")
+print("69")
+print(1)
+print(35)
+print(99)
+print(2.1)
+print(2.5)
+print(6.9)
+
+
+
+number1 ="21"
+number2 ="69"
+
+
+print(int(number1) + int(number2))
+
+
+input(help)
---
+++ level 14/classwork/class.py
@@ -0,0 +1,18 @@
+number1 =  169
+number2 = 25
+number3 = 2.5
+number4 =  12
+number5 = 12
+number6 = "hello world"
+
+
+
+print (type(number1)) # class =int
+print (type(number2)) # class = int
+print (type(number3)) # class = float
+print (type(number4)) # class = int
+print (type(number5)) # class = float
+print (type(number6)) # class = str
+
+
+
