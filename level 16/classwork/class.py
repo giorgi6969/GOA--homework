6ec5989a0f09b633bae5933790e366af73ffaf3e
giorgi6969 <qweqwe@gmail.com>
2024-08-30 16:51:33 +0400

    second commit

---
+++ level 16/classwork/class.py
@@ -0,0 +1,68 @@
+a = 3.5
+ 
+b = 5
+
+c = 10
+ 
+
+
+
+
+
+
+print(b > a)
+print(c > a)
+print(a > c)
+print(c > b)
+
+
+print(a < c)
+print(b < a)
+print(b < c)
+print(a < b)
+
+
+
+print(c >= c)
+print(c >= b)
+print(a >= a)
+print(b >= b)
+
+
+print(b <= b)
+print(c <= c)
+print(a <= a)
+print(b <= c)
+
+
+print(c == c)
+print(b == b)
+print(a == a)
+print(a == c)
+
+
+print(a != a)
+print(b != b)
+print(c != c)
+print(c != a)
+
+
+
+
+
+
+
+num1 =int(input("enter your number 1;"))
+
+
+num2 =int(input("enter your number 2;"))
+
+
+
+
+print(num1 > num2)
+print(num2 < num1)
+print(num2 <= num1)
+print(num2 >= num1)
+print(num2 != num2)
+print(num1 == num1)
---
+++ level 16/homework/homework.py
@@ -0,0 +1,59 @@
+
+# homework 1
+
+# binary code = ბინარული კოდი ბინარული კოდი არის ინფორმაციის აღსაწერად გამოყენებული კოდის სისტემა, რომელიც მხოლოდ ორი სიმბოლოს, 0 და 1, იყენებს. ეს სისტემა ძირითადი ფორმაა მონაცემების და ინსტრუქციების გამოსახატავად კომპიუტერების და სხვა ციფრული სისტემების მიერ.
+# ბულეან (Boolean) ნიშნავს ლოგიკურ ტიპს, რომელიც გამოიყენება ორი ძირითადი მდგომარეობის გამოსახატავად: True (სიმართლე) და False (ტყუილი). ეს კონცეფცია ფართოდ გამოიყენება კომპიუტერულ მეცნიერებაში, პროგრამირებაში და ლოგიკაში.
+
+#  homework 2
+
+a = 3.5
+ 
+b = 5
+
+c = 10
+ 
+
+
+
+
+
+
+print(b > a)
+print(c > a)
+print(a > c)
+print(c > b)
+print(c > c)
+
+print(a < c)
+print(b < a)
+print(b < c)
+print(a < b)
+print(c < a)
+
+
+print(c >= c)
+print(c >= b)
+print(a >= a)
+print(b >= b)
+print(c >= a)
+
+print(b <= b)
+print(c <= c)
+print(a <= a)
+print(b <= c)
+print(c <= b)
+
+print(c == c)
+print(b == b)
+print(a == a)
+print(a == c)
+print(b == c)
+
+print(a != a)
+print(b != b)
+print(c != c)
+print(c != a)
+print(c != b)
+
+
+
