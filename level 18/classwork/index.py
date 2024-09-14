85da96719dea33633479a033cfe0ae98af7c4a42
giorgi6969 <qweqwe@gmail.com>
2024-09-14 13:12:30 +0400

    second commit

---
+++ level 18/classwork/index.py
@@ -0,0 +1,8 @@
+#(true and true) = true
+#(true or false) = true
+#(true and false) = false
+#(false and true) = false
+#(false or false) = flase
+
+
+
---
+++ level 18/homework/hm.py
@@ -0,0 +1,21 @@
+# and ოპერატორი უპრო არი მიგრეკილი ფალსთან ამიტომაც იყენება რომ ყოველტვის ფალსი გაბოიტანოს მაგალითად:5 > 6 10 > 7 ამ შემთხვევაში ის გამოიტანს 5 > 6 იმიტომ რომ and მიგრეკილია ფალსთან(არასწორთან)
+# or ოპერატორი უპრო არი მიგრეკილი თრუსთან ამიტომაც იყენება რომ ყოველტვის თრუ გაბოიტანოს მაგალითად:5 > 6 10 > 7 ამ შემთხვევაში ის გამოიტანს 10 > 7 იმიტომ რომ or მიგრეკილია თრუ(სწორითან)
+
+
+
+# print((True or False) and (True and True))
+# print((True or False) and (False or False))
+# print((False and True) or (True and False))
+# print((False or False) and (True or True))
+# print((True and True) or (False and True))
+# print((False or True) and (True and False))
+# print((False and False) or (False or True))
+# print((True or False) and (False or True))
+# print((True and False) or (True and False))
+# print((True and True) or (True and False))
+# print((False and False) or (True or bool(5)))
+# print((True or True) and (False and False))
+# print((False or True) and (True or True))
+# print((True and False) or (True and False))
+
+print(True or False and True and True)
