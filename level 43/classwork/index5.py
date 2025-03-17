9fe9d3736fb573a292bf8f965b29c76e411098b9
giorgi6969 <qweqwe@gmail.com>
2025-03-17 21:41:46 +0400

    second commit

---
+++ level 43/classwork/index5.py
@@ -0,0 +1,11 @@
+def complex_calc(a, b):
+    if a % 2 == 0:  
+        return a + b
+    else:          
+        return a * b
+
+
+print(complex_calc(4, 5))  
+print(complex_calc(3, 5))  
+print(complex_calc(10, 2)) 
+print(complex_calc(7, 2))  
